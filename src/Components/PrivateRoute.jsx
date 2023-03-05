63405ec326124570066573322950100ff3da25e4
TheTagEnd <vashudev8777@gmail.com>
2023-03-05 17:33:47 +0530

    spinner component added

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import  Spinner from "./Spinner"
 import { Navigate, Outlet } from "react-router-dom";
 import {useAuthStatus}  from "../hooks/useAuthStatus";
 export default function PrivateRoute() {
      const {loggedIn , checkingStatus } =useAuthStatus()
          if(checkingStatus ){
-            return <h3>Loading...</h3>
+            return  <Spinner ></Spinner>
          }     
     return   loggedIn ? <Outlet /> : <Navigate to  = "/sign_in"/>
   
